8fc9def219d383de56bcc6ee2eca42ebb9b54f38
Cody Skidmore <4048971+codyskidmore@users.noreply.github.com>
2023-08-31 12:42:30 -0400

    Reorganized folders.

--- src/minimal-api/Minimal.Api/Data/WeatherData.cs
+++
@@ -1,9 +0,0 @@
-namespace Minimal.Api.Data;
-
-public static class WeatherData
-{
-    public static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-}
